b7666b1c753519ae7510e5fe4c010d3c4e7b49de
Elena Sidanova <l.sidan@gmx.de>
2024-11-15 08:58:15 +0100

    correct styles

--- src/features/salads/SaladsModal.tsx
+++ src/features/salads/SaladsModal.tsx
@@ -1,6 +1,5 @@
 import { useState } from 'react';
 import styles from '../pasta-items/pastaModal.module.css';
-
 import { t } from 'i18next';
 import { useDispatch } from 'react-redux';
 import { addItem } from '../cart-items/cartSlice';
